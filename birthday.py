69929a37512639153db2c7f9998e7be14c9cd22c
Doalina <125969411+Doalina@users.noreply.github.com>
2023-04-22 23:02:58 +0200

    Update birthday.py

--- birthday.py
+++ birthday.py
@@ -1,64 +1,54 @@
-from datetime import datetime, timedelta
+from datetime import date, timedelta
 
 users = [
-    {"name": "John", "birthday": datetime(1998, 5, 12)},
-    {"name": "Sarah", "birthday": datetime(2000, 5, 21)},
-    {"name": "Michael", "birthday": datetime(1995, 5, 7)},
-    {"name": "Emily", "birthday": datetime(1994, 5, 15)},
-    {"name": "William", "birthday": datetime(1987, 5, 25)},
-    {"name": "Olivia", "birthday": datetime(1999, 5, 3)},
-    {"name": "Jacob", "birthday": datetime(1992, 5, 30)},
-    {"name": "Matthew", "birthday": datetime(1991, 5, 9)},
-    {"name": "Hannah", "birthday": datetime(2002, 5, 1)},
-    {"name": "Victoria", "birthday": datetime(1993, 5, 5)},
-    {"name": "Andrew", "birthday": datetime(1996, 5, 14)},
-    {"name": "Sophia", "birthday": datetime(1998, 5, 22)},
-    {"name": "David", "birthday": datetime(1997, 5, 1)},
-    {"name": "Isabella", "birthday": datetime(1994, 5, 12)},
-    {"name": "Benjamin", "birthday": datetime(2001, 5, 23)}
+    {"name": "John", "birthday": date(1996, 4, 22)},
+    {"name": "Sarah", "birthday": date(2000, 4, 26)},
+    {"name": "Michael", "birthday": date(1995, 4, 29)},
+    {"name": "Emily", "birthday": date(1994, 4, 30)},
+    {"name": "William", "birthday": date(1987, 4, 25)},
+    {"name": "Olivia", "birthday": date(1999, 4, 24)},
+    {"name": "Jacob", "birthday": date(1991, 4, 28)},
+    {"name": "Matthew", "birthday": date(1991, 4, 23)},
+    {"name": "Hannah", "birthday": date(2002, 4, 27)},
+    {"name": "Victoria", "birthday": date(1993, 4, 21)}
 ]
 
 def get_birthdays_per_week(users):
-    # Отримуємо поточну дату
-    today = datetime.today().date()
-    current_weekday = today.weekday()
+    #  Отримуємо поточну дату
+    today = date.today()
+    monday = today - timedelta(days=today.weekday())
+    next_monday = monday + timedelta(days=7)
 
-    # створюємо словник для кожного дня тижня
-    birthdays_by_weekday = {
-        0: [],
-        1: [],
-        2: [],
-        3: [],
-        4: [],
-        5: [],
-        6: []
-    }
-
-    # Дадаємо користувачів у відповідний день тижня
+    # проходимо по списку і перевіряємо їх дні народження
+    birthday_list = []
     for user in users:
-        birthday = user['birthday']
-        if birthday.weekday() >= 5:
-            birthday = birthday + timedelta(days=7 - birthday.weekday())
-        birthdays_by_weekday[birthday.weekday()].append(user['name'])
+        birthday = user["birthday"].replace(year=today.year)
 
-    # Роздруковуємо дні народження по днях тижня
-    for i in range(7):
-        weekday = (current_weekday + i + 1) % 7
-        weekday_name = datetime.strptime(f"2023-04-{3+weekday}", "%Y-%m-%d").strftime('%A')
-        if birthdays_by_weekday[weekday]:
-            print(f"{weekday_name}: {', '.join(birthdays_by_weekday[weekday])}")
+        # якщо день народження вже пройшов цього року
+        if birthday < today:
+            birthday = user["birthday"].replace(year=today.year + 1)
 
+        # якщо день народження випадає на поточний тиждень
+        if monday <= birthday <= monday + timedelta(days=6):
+            if birthday.weekday() >= 5:
+                birthday_list.append({"name": user["name"], "birthday": next_monday})
+            else:
+                birthday_list.append({"name": user["name"], "birthday": birthday})
+
+        # якщо день народження припадає на наступний тиждень і не в суботу / неділю
+        elif next_monday <= birthday <= next_monday + timedelta(days=4):
+            if birthday.weekday() < 5:
+                birthday_list.append({"name": user["name"], "birthday": birthday})
+
+    # сортуємо список по датах народження
+    birthday_list.sort(key=lambda x: x["birthday"])
+
+    # виводимо список імен по днях тижня
+    for day in range(7):
+        date_str = (monday + timedelta(days=day)).strftime("%A")
+        names = [x["name"] for x in birthday_list if x["birthday"].weekday() == day]
+        if names:
+            print(f"{date_str}: {', '.join(names)}")
 
 if __name__ == '__main__':
     get_birthdays_per_week(users)
-
-
-
-
-
-
-
-
-
-
-
